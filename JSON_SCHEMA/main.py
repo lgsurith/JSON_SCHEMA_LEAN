736a2d1440c48b196e9451ff888bb4397d855f52
Surith <117572209+lgsurith@users.noreply.github.com>
2024-02-25 07:53:13 +0530

    Update main.py

--- JSON_SCHEMA/main.py
+++ JSON_SCHEMA/main.py
@@ -2,10 +2,12 @@ import json
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
-with open(r'C:\Users\lgpoo\OneDrive\Desktop\GSOC_2024\JSON_SCHEMA_LEAN\JSON_SCHEMA\test1.json') as f:
+#give your file to be tested as a json.
+with open(r'C:\Users\Edith\OneDrive\Desktop\GSOC_2024\JSON_SCHEMA_LEAN\JSON_SCHEMA\test1.json') as f:
     document = json.load(f)
 
-with open(r'C:\Users\lgpoo\OneDrive\Desktop\GSOC_2024\JSON_SCHEMA_LEAN\JSON_SCHEMA\create_new.json') as f:
+#give your path for the json schema used to validate the json.
+with open(r'C:\Users\Edith\OneDrive\Desktop\GSOC_2024\JSON_SCHEMA_LEAN\JSON_SCHEMA\create_new.json') as f:
     schema = json.load(f)
 
 try:
